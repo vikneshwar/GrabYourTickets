b955fa8fb2368285493070334c731528ceb8f299
Vikneshwar <lviknesh@gmail.com>
2016-08-05 02:24:14 +0530

    temp clock update

--- clock.js
+++ clock.js
@@ -2,7 +2,7 @@ var CronJob = require('cron').CronJob;
 
 var checkAvailable = require('./jobs/checkAvailable.js');
 var job = new CronJob({
-	cronTime: '*/15 * * * *',
+	cronTime: '*/1 * * * *',
 	onTick: checkAvailable.initiateCheck,
 	start: true
 });
